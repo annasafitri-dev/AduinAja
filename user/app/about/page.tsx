4aba5f5b3a36cd004c2987c3cc4ea7631872ae6f
annasafitri-dev <anna_safitri@teknokrat.ac.id>
2026-06-10 22:30:27 +0700

    tambah deskripsi

---
+++ user/app/about/page.tsx
@@ -0,0 +1,16 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>Tentang Kami</h1>
+
+        <p>
+        <b>AduinAja!</b> merupakan platform pengaduan masyarakat berbasis digital.
+        </p>
+        <p>
+        Memudahkan masyarakat dalam menyampaikan laporan secara cepat dan aman.
+        </p>
+    </div>
+
+    
+  );
+}
