2abe0032e632610bc62281c231491688f1407236
annasafitri-dev <anna_safitri@teknokrat.ac.id>
2026-06-13 23:08:57 +0700

    feat: perbaiki eror

--- cms/app/admin/page.tsx
+++ cms/app/admin/page.tsx
@@ -156,9 +156,6 @@ export default function AdminPage() {
               {new Date().toLocaleDateString("id-ID")}
             </p>
 
-            <p style={{ margin: 0 }}>
-                {new Date().toLocaleTimeString("id-ID")}
-              </p>
             </div>
           </div>
 
