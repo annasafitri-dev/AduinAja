67ce6c628fece2380f871976e6d668862c05de0b
annasafitri-dev <anna_safitri@teknokrat.ac.id>
2026-06-13 21:37:08 +0700

    feat: menambahkan form login

--- user/app/login/page.tsx
+++ user/app/login/page.tsx
@@ -1,17 +1,24 @@
-"use client";
-
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
   const router = useRouter();
 
-  function handleLogin() {
-    router.push("/");
-  }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  return (
-    <button onClick={handleLogin}>
-      Login
-    </button>
-  );
-}
+  const handleLogin = () => {
+    if (
+      email === "annasafitri@gmail.com" &&
+      password === "123456"
+    ) {
+      localStorage.setItem("isLogin", "true");
+      localStorage.setItem("nama", "Anna Safitri");
+      localStorage.setItem("email", email);
+      localStorage.setItem("telepon", "081234567890");
+
+      router.push("/");
+    } else {
+      alert("Email atau password salah!");
+    }
+  };
